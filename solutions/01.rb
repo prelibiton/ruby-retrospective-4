c318a3a992f194dec8f70c0191684e2f7f2cb15d
prelibiton <rainship@gmail.com>
2015-01-16 23:26:25 +0200

    function name changed
    changed get_element to generic_series

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,14 +1,15 @@
-def get_element(first, second, index)
-  return first if index <= 1
+def generic_series(first, second, index)
+  return first  if index == 1
   return second if index == 2
 
-  get_element(first, second, index - 1) + get_element(first, second, index - 2)
+  generic_series(first, second, index - 1) +
+  generic_series(first, second, index - 2)
 end
 
 def series(name, index)
   case name
-    when  'fibonacci' then get_element(1, 1, index)
-    when  'lucas'     then get_element(2, 1, index)
-    else get_element(1, 1, index) + get_element(2, 1, index)
+    when  'fibonacci' then generic_series(1, 1, index)
+    when  'lucas'     then generic_series(2, 1, index)
+    else generic_series(1, 1, index) + generic_series(2, 1, index)
   end
 end
